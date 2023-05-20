3678abdf8aa0ca243ec154aa89cdf1711aae965c
ahmad-frehan <ahmad-frehan@futurecode-co.com>
2023-05-20 22:04:22 +0300

    init

--- src/hellofx/App.java
+++ src/hellofx/App.java
@@ -3,8 +3,6 @@ package hellofx;
 import java.io.File;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
